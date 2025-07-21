064193a972956b5ffcfc2826e10d84103cd7a728
Jimisola Laursen <jimisola@jimisola.com>
2025-07-21 19:19:10 +0200

    feat: add PROGRAM_UPDATED event type to EventType class

--- src/backend/common/event_type.py
+++ src/backend/common/event_type.py
@@ -11,6 +11,7 @@ class EventType:
     CHRONO: str = "chrono"  #
     PROGRAM_ADDED: str = "program_added"
     PROGRAM_DELETED: str = "program_deleted"
+    PROGRAM_UPDATED: str = "program_updated"§
     AUDIO_ADDED: str = "audio_added"
     AUDIO_DELETED: str = "audio_deleted"
     BACKEND_ISSUE: str = "backend_issue"
